1c68c472a49d6ed008f00e524ef627d61636fdcd
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-27 14:21:10 +0500

    getting information about employee 1

--- Salary Calculator/Program.cs
+++ Salary Calculator/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Salary
+{
+    static void Main(string[] args)
+    {
+        // declaring variables
+        string employee1, employee2, employee3;
+        double hours1, hours2, hours3;
+        double rate1, rate2, rate3;
+
+        // taking input form user for employee 1
+        Console.Write("Enter Name For Employee 1: ");
+        employee1 = Console.ReadLine();
+
+        Console.Write("Enter Working Hours For Employee 1: " );
+        hours1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Rate Per Hour: ");
+        rate1 = Convert.ToDouble(Console.ReadLine());
+    }
+}
+
