20a225848fc6dca04f3dd6718e5460803dcbffae
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-27 19:26:27 +0500

    getting employee 1 and 2 information from user

--- Salary Calculator/Program.cs
+++ Salary Calculator/Program.cs
@@ -14,8 +14,28 @@
         Console.Write("Enter Working Hours For Employee 1: " );
         hours1 = Convert.ToDouble(Console.ReadLine());
 
-        Console.Write("Enter Rate Per Hour: ");
+        Console.Write("Enter Rate Per Working Hour Of Employee 1: ");
         rate1 = Convert.ToDouble(Console.ReadLine());
+
+        // taking input form user for employee 2
+        Console.Write("Enter Name For Employee 2: ");
+        employee2 = Console.ReadLine();
+
+        Console.Write("Enter Working Hours For Employee 2: ");
+        hours2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Rate Per Working Hour Of Employee 2: ");
+        rate2 = Convert.ToDouble(Console.ReadLine());
+
+        // taking input form user for employee 3
+        Console.Write("Enter Name For Employee 3: ");
+        employee3 = Console.ReadLine();
+
+        Console.Write("Enter Working Hours For Employee 3: ");
+        hours3 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Rate Per Working Hour Of Employee 3: ");
+        rate3 = Convert.ToDouble(Console.ReadLine());
     }
 }
 
